f86f5e984e49e53a7d0740936834f03159b9e7ad
Junichi Kobayashi <nnujabok@gmail.com>
2017-07-07 14:13:26 +0900

    add database settings

--- src/main/scala/jp/co/esminc/HelloWorldServlet.scala
+++ src/main/scala/jp/co/esminc/HelloWorldServlet.scala
@@ -1,14 +1,27 @@
 package jp.co.esminc
 
 import org.scalatra._
+import slick.driver.MySQLDriver.api._
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 class HelloWorldServlet extends HelloworldStack {
 
   get("/") {
+     val db = Database.forURL("jdbc:mysql://104.198.91.161/frm", driver="com.mysql.jdbc.Driver", user="root")
+    val query = sql"SELECT id, name FROM frinds".as[(Int, String)]
+    val f:Future[Vector[(Int, String)]] = db.run(query)
+            Await.result(f, Duration.Inf) foreach println
+
+    
     <html>
       <body>
         <h1>Hello, world!</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
+
+        Await.result(f, Duration.Inf) foreach println
+
       </body>
     </html>
   }
